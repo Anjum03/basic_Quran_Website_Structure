38d0365b2c0ceb65c59dc49e21dc9e7690f49c8e
Anjum <anjum2000ansari@gmail.com>
2023-05-22 17:59:20 +0530

    first commit

--- model/loginModel.js
+++ model/loginModel.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 const loginSchema = new mongoose.Schema({
     email: {type: String},
     password: {type: String,min: 8,},
-    role: {type:String, enum:['user', 'admin'], default: 'user', }
+    role: {type:String, enum:['user', 'admin'], default: 'user', },
+    token:{ type: String, default : " "}
 }, { timestamps : true }
 );
 
--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -5,6 +5,8 @@ const router = require('express').Router();
 const Login = require('../model/loginModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+const randomstring = require('randomstring');
 
 
 //Login
@@ -31,7 +33,7 @@ router.post('/login', async(req,res)=>{
 
                 res.status(200).json({success: true, data: foundUser, message: isAdmin ? `Admin Login Successfully` : `User Login SuccessFully :)`,  isAdmin, token,})
             } else{
-                res.status(401).json({ success: false, msg: `Invalid Email or password`})
+                res.status(401).json({ success: false, msg: `Wrong password`})
             }
 
         }  else {
@@ -58,6 +60,153 @@ router.post('/login', async(req,res)=>{
 });
 
 
+// //sendPasswordlink
+// router.post('/sendPasswordLink', async(req,res)=>{
+
+//     const {email} = req.body ;
+    
+//     try{
+
+//         const user = await Login.findOne({email});
+
+//         if(!user){
+//             res.status(400).json({ success : false, msg: `User Not Found`})
+//         }
+
+//         //generate jwt token 
+//         const token = jwt.sign({id:user._id}, process.env.JWT_SECRET_KEY,{ expiresIn: '180s'})
+
+//     } catch(error){
+//         console.log(error);
+//         res.status(500).json({ success: false, msg:`Server Error`})
+//     }
+
+// });
+
+
+//forgot Password
+
+// Send password reset email
+function sendResetPasswordEmail (email,token ){
+    try {
+
+        const transporter = nodemailer.createTransport({
+            //configure
+            service: 'Gmail',
+            auth:{
+                user: process.env.USEREMAIL,
+                pass: process.env.PASSWORD,
+            },
+    
+        })
+        
+        const mailOptions = {
+            from: process.env.USEREMAIL,
+            to : email, 
+            subject: "Reset Passowrd",
+            text: " reset Password Link ",
+            html: '<p> Hi '+email+' , PLease Copy the link and <a href =" http://localhost:3000/resetPassword?token='+token+'" > reset the password </a>'
+        };
+
+        transporter.sendMail(mailOptions, (error, info)=>{
+            if(error){
+                console.log(error);
+            } else{
+                console.log('Email, sent : ' + info.response)
+            }
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ success: false, msg: `Server Error`})
+    }
+
+}
+
+router.post('/forgotPassword', async(req,res)=>{
+      // step: 1. check exist email 
+//         //2.
+//         //3. send link on email for reset password  (like : localhost :3000/resetPasssword/userId/token)
+//         //4. now get Request localhost link with userID and token (like : localhost:3000/resetPassord/:id/:token) id = userid and token = jwt token 
+//         //
+
+try{
+        const email  = req.body.email; 
+
+        const user = await Login.findOne({ email: email }) ;
+
+        if(user){ //user Exist
+
+            const randomString = randomstring.generate(7) // --> 7 means only seven alpha pbet is present
+
+            const updateTokeninDB = await Login.updateOne({email:email}, {$set: {token: randomString}}, {new: true});
+            //send email part
+            sendResetPasswordEmail(user.email,randomString); 
+
+            res.status(200).json({ success: true, msg:`Please check your Email and Reset Password`})
+            
+            res.status(400).json({ success: false, msg:`User Not Found`});
+        } else{ //user not exist
+            res.status(400).json({ success: false, msg: `This Email Doesn't Exist . Please Login Again`})
+        }
+
+        //send link on email --> click on link and redirect to enter your new paasword and redirect to home 
+//send email part 
+//done it make as a function
+
+return res.status(200).json({ success: true, msg:`Reset Password email Sent`})
+//inside email one link to add new password 
+
+    } catch(error){
+        res.status(500).json({ success: false , msg : `Server Error `})
+    }
+
+});
+
+
+//reset Password
+router.post('/resetPassword', async(req,res)=>{
+
+});
+
+
+
+//change password
+router.post('/changePassword', async(req,res)=>{
+
+    const {email, newPassword, oldPassword} = req.body ;
+
+    try{
+
+        const user = await Login.findOne({email});
+
+        if(!user){
+            return res.status(400).json({ success: false, msg: `User Not Found`});
+        }
+
+        const isMatch = await bcrypt.compare (oldPassword, user.password)
+        if(!isMatch){
+            return res.status(401).json({ success: false , msg: `Your Old Password is Wrong !!!`})
+        } else{
+
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(newPassword, salt);
+            user.password = hashedPassword ;
+            await user.save();
+            return res.status(200).json({ success: true, message: 'Password changed successfully!' });
+        }
+
+        // const code = crypto.randomBytes(20).toString('hex');
+
+        //update the user's [Password and clear the forgotPasswordCode in DB]
+
+    } catch (error){
+        console.log(error);
+        return res.status(500).json({ success: false, msg:`Server Error`})
+    }
+
+});
+
 
 // logout
 router.post('/logout', async(req,res)=>{
@@ -79,19 +228,29 @@ router.put('/update/:id', async(req,res)=>{
     try {
         const existingUser = await Login.findOne({ email: updateData.email });
     
-        if (existingUser && existingUser._id.toString() !== userId) {
-          return res.status(400).json({ success: false, Msg: 'Please Enter Unique Email-Id' });
-        }
-    
-        const updateUser = await Login.findByIdAndUpdate(userId, updateData, { new: true });
-    
-        if (!updateUser) {
+        if (!existingUser) {
           return res.status(400).json({ success: false, Msg: 'User Not Found' });
+        } else{
+
+            if (existingUser && existingUser._id.toString() !== userId) {
+              return res.status(400).json({ success: false, Msg: 'Please Enter Unique Email-Id' });
+            }
+            
+            if( updateData.password ){
+                const salt = 10 
+                const newHashedPassword = await bcrypt.hash( updateData.password, salt);
+                updateData.password = newHashedPassword ;
+            }
+       
+
+            const updateUser = await Login.findByIdAndUpdate(userId, updateData, { new: true });
+        
+            res.status(200).json({ success: true, msg: 'User Update Successfully', data: updateUser });
+          
         }
-    
-        res.status(200).json({ success: true, msg: 'User Update Successfully', data: updateUser });
-      
+
     } catch(error){
+        console.log(error)
         res.status(500).json({ success: false, msg: `Server Error`})
     }
 
